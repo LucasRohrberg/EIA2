07a19407ca20ba83a866cc21914d1a9a26b2d123
LucasRohrberg <44026433+LucasRohrberg@users.noreply.github.com>
2019-04-10 18:04:50 +0200

    added sorting mechanism

--- task-2/js/main.js
+++ task-2/js/main.js
@@ -1,23 +1,52 @@
+/*
+Aufgabe: <2 - Mau Mau>
+Name: <Lucas Rohrberg>
+Matrikel: <260241>
+Datum: <7. April 2019, 12:39Uhr>
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
 let handCards = [];
-let Deck = [{ type: "hearts", score: "7" }, { type: "hearts", score: "8" }, { type: "hearts", score: "9" }, { type: "hearts", score: "10" }, { type: "hearts", score: "jack" }, { type: "hearts", score: "queen" }, { type: "hearts", score: "king" }, { type: "hearts", score: "ace" },
-    { type: "diamonds", score: "7" }, { type: "diamonds", score: "8" }, { type: "diamonds", score: "9" }, { type: "diamonds", score: "10" }, { type: "diamonds", score: "jack" }, { type: "diamonds", score: "queen" }, { type: "diamonds", score: "king" }, { type: "diamonds", score: "ace" },
-    { type: "spades", score: "7" }, { type: "spades", score: "8" }, { type: "spades", score: "9" }, { type: "spades", score: "10" }, { type: "spades", score: "jack" }, { type: "spades", score: "queen" }, { type: "spades", score: "king" }, { type: "spades", score: "ace" },
-    { type: "clubs", score: "7" }, { type: "clubs", score: "8" }, { type: "clubs", score: "9" }, { type: "clubs", score: "10" }, { type: "clubs", score: "jack" }, { type: "clubs", score: "queen" }, { type: "clubs", score: "king" }, { type: "clubs", score: "ace" }];
+let newCard = "";
+let cardAmount;
+let Deck = [{ type: "hearts", order: 0, score: "7" }, { type: "hearts", order: 1, score: "8" }, { type: "hearts", order: 2, score: "9" }, { type: "hearts", order: 3, score: "10" }, { type: "hearts", order: 4, score: "jack" }, { type: "hearts", order: 5, score: "queen" }, { type: "hearts", order: 6, score: "king" }, { type: "hearts", order: 7, score: "ace" },
+    { type: "diamonds", order: 8, score: "7" }, { type: "diamonds", order: 9, score: "8" }, { type: "diamonds", order: 10, score: "9" }, { type: "diamonds", order: 11, score: "10" }, { type: "diamonds", order: 12, score: "jack" }, { type: "diamonds", order: 13, score: "queen" }, { type: "diamonds", order: 14, score: "king" }, { type: "diamonds", order: 15, score: "ace" },
+    { type: "spades", order: 24, score: "7" }, { type: "spades", order: 25, score: "8" }, { type: "spades", order: 26, score: "9" }, { type: "spades", order: 27, score: "10" }, { type: "spades", order: 28, score: "jack" }, { type: "spades", order: 29, score: "queen" }, { type: "spades", order: 30, score: "king" }, { type: "spades", order: 31, score: "ace" },
+    { type: "clubs", order: 16, score: "7" }, { type: "clubs", order: 17, score: "8" }, { type: "clubs", order: 18, score: "9" }, { type: "clubs", order: 19, score: "10" }, { type: "clubs", order: 20, score: "jack" }, { type: "clubs", order: 21, score: "queen" }, { type: "clubs", order: 22, score: "king" }, { type: "clubs", order: 23, score: "ace" }];
 function getCardAmount() {
-    let enterCardAmount = prompt("Enter the amount of cards the player gets:");
-    let cardAmount = Number(enterCardAmount);
+    let enterCardAmount = prompt("Choose a number between 4 and 6");
+    cardAmount = Number(enterCardAmount);
+    if (isNaN(cardAmount) == true || cardAmount < 4 || cardAmount > 6)
+        getCardAmount();
     return cardAmount;
 }
 function dealCards(_handCardsTotal) {
-    let newCard = "";
     for (let i = 0; i < _handCardsTotal; i++) {
         let randomCard = Math.floor(Math.random() * Deck.length);
         handCards.push(Deck[randomCard]);
-        console.log(handCards[i]);
-        newCard += `<div id="i${i}" class="${handCards[i].type}">${handCards[i].score} of ${handCards[i].type}</div>`;
         Deck.splice(randomCard, 1);
+        writeHtml(i);
     }
+}
+function sortCards() {
+    handCards.sort(function (a, b) {
+        return a.order - b.order;
+    });
+}
+function sortDisplay() {
+    sortCards();
+    document.getElementById("handCards").innerHTML = "";
+    newCard = "";
+    for (let i = 0; i < handCards.length; i++) {
+        writeHtml(i);
+    }
+}
+function writeHtml(_i) {
+    newCard += `<div class="${handCards[_i].type}">${handCards[_i].score} of ${handCards[_i].type}</div>`;
     document.getElementById("handCards").innerHTML = newCard;
 }
+function playCard() {
+}
 dealCards(getCardAmount());
+document.getElementById("sortCards").addEventListener("click", sortDisplay);
 //# sourceMappingURL=main.js.map
--- task-2/js/main.ts
+++ task-2/js/main.ts
@@ -10,29 +10,58 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 interface Card {
     type: string;
     score: string;
+    order: number;
 }
 
 let handCards: Card[] = [];
+let newCard: string = "";
+let cardAmount : number;
 
-let Deck: Card[] = [{ type: "hearts", score: "7" }, { type: "hearts", score: "8" }, { type: "hearts", score: "9" }, { type: "hearts", score: "10" }, { type: "hearts", score: "jack" }, { type: "hearts", score: "queen" }, { type: "hearts", score: "king" }, { type: "hearts", score: "ace" },
-{ type: "diamonds", score: "7" }, { type: "diamonds", score: "8" }, { type: "diamonds", score: "9" }, { type: "diamonds", score: "10" }, { type: "diamonds", score: "jack" }, { type: "diamonds", score: "queen" }, { type: "diamonds", score: "king" }, { type: "diamonds", score: "ace" },
-{ type: "spades", score: "7" }, { type: "spades", score: "8" }, { type: "spades", score: "9" }, { type: "spades", score: "10" }, { type: "spades", score: "jack" }, { type: "spades", score: "queen" }, { type: "spades", score: "king" }, { type: "spades", score: "ace" },
-{ type: "clubs", score: "7" }, { type: "clubs", score: "8" }, { type: "clubs", score: "9" }, { type: "clubs", score: "10" }, { type: "clubs", score: "jack" }, { type: "clubs", score: "queen" }, { type: "clubs", score: "king" }, { type: "clubs", score: "ace" }];
+let Deck: Card[] = [{ type: "hearts", order: 0, score: "7" }, { type: "hearts", order: 1, score: "8" }, { type: "hearts", order: 2, score: "9" }, { type: "hearts", order: 3, score: "10" }, { type: "hearts", order: 4, score: "jack" }, { type: "hearts", order: 5, score: "queen" }, { type: "hearts", order: 6, score: "king" }, { type: "hearts", order: 7, score: "ace" },
+{ type: "diamonds", order: 8, score: "7" }, { type: "diamonds", order: 9, score: "8" }, { type: "diamonds", order: 10, score: "9" }, { type: "diamonds", order: 11, score: "10" }, { type: "diamonds", order: 12, score: "jack" }, { type: "diamonds", order: 13, score: "queen" }, { type: "diamonds", order: 14, score: "king" }, { type: "diamonds", order: 15, score: "ace" },
+{ type: "spades", order: 24, score: "7" }, { type: "spades", order: 25, score: "8" }, { type: "spades", order: 26, score: "9" }, { type: "spades", order: 27, score: "10" }, { type: "spades", order: 28, score: "jack" }, { type: "spades", order: 29, score: "queen" }, { type: "spades", order: 30, score: "king" }, { type: "spades", order: 31, score: "ace" },
+{ type: "clubs", order: 16, score: "7" }, { type: "clubs", order: 17, score: "8" }, { type: "clubs", order: 18, score: "9" }, { type: "clubs", order: 19, score: "10" }, { type: "clubs", order: 20, score: "jack" }, { type: "clubs", order: 21, score: "queen" }, { type: "clubs", order: 22, score: "king" }, { type: "clubs", order: 23, score: "ace" }];
 
 function getCardAmount(): number {
-    let enterCardAmount: string = prompt("Enter the amount of cards the player gets:");
-    let cardAmount: number = Number(enterCardAmount);
+    let enterCardAmount : string = prompt("Choose a number between 4 and 6");
+    cardAmount = Number(enterCardAmount);
+    if (isNaN(cardAmount) == true || cardAmount < 4 || cardAmount > 6) getCardAmount();
     return cardAmount;
 }
 
-function dealCards(_handCardsTotal: number) { 
-    let newCard: string = "";
+function dealCards(_handCardsTotal: number) {
     for (let i = 0; i < _handCardsTotal; i++) {
         let randomCard: number = Math.floor(Math.random() * Deck.length);
         handCards.push(Deck[randomCard]);
-        newCard += `<div class="${handCards[i].type}">${handCards[i].score} of ${handCards[i].type}</div>`;
         Deck.splice(randomCard, 1);
+        writeHtml(i);
     }
+}
+
+function sortCards() {
+    handCards.sort(function (a, b) {
+        return a.order - b.order;
+    });
+}
+
+function sortDisplay() {
+    sortCards();
+    document.getElementById("handCards").innerHTML = "";
+    newCard = "";
+    for (let i = 0; i < handCards.length; i++) {
+        writeHtml(i);
+    }
+}
+
+function writeHtml(_i:number) {
+    newCard += `<div class="${handCards[_i].type}">${handCards[_i].score} of ${handCards[_i].type}</div>`;
     document.getElementById("handCards").innerHTML = newCard;
 }
+
+function playCard() {
+
+}
+
 dealCards(getCardAmount());
+
+document.getElementById("sortCards").addEventListener("click", sortDisplay);
