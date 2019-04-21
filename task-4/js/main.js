fb827433c98df2a947becc621a44e62a8ffcf048
LucasRohrberg <muffinamv@gmail.com>
2019-04-21 13:03:31 +0200

    basic functionality
    adding and removing ice & extras from the order work, the sum updates accordingly as well
    
    to-do: slider for ice cream, design stuff, check-order-button

--- task-4/js/main.js
+++ task-4/js/main.js
@@ -1,23 +1,19 @@
-namespace A4 {
+var A4;
+(function (A4) {
     window.addEventListener("load", init);
-
-    let sum: number = 0;
-    let customerOrder: string = "";
-
-    function init(_event: Event): void {
+    let sum = 0;
+    let customerOrder = "";
+    function init(_event) {
         console.log("Init");
-        let fieldsets: HTMLCollectionOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
-
-        for (let i: number = 0; i < fieldsets.length; i++) {
-            let fieldset: HTMLFieldSetElement = fieldsets[i];
+        let fieldsets = document.getElementsByTagName("fieldset");
+        for (let i = 0; i < fieldsets.length; i++) {
+            let fieldset = fieldsets[i];
             fieldset.addEventListener("change", handleChange);
         }
     }
-
-    function handleChange(_event: Event) {
-        let target: HTMLInputElement = <HTMLInputElement>_event.target;
-        let price: number;
-
+    function handleChange(_event) {
+        let target = _event.target;
+        let price;
         if (target.name == "ice" && target.checked == true) {
             price = Number(target.getAttribute("price"));
             sum += price;
@@ -45,12 +41,11 @@ namespace A4 {
             updateSum(sum);
         }
     }
-
-    function updateCustomerOrder(_customerOrder: string) {
+    function updateCustomerOrder(_customerOrder) {
         document.getElementById("orderSummary").innerHTML += _customerOrder;
     }
-
-    function updateSum(sum: number) {
+    function updateSum(sum) {
         document.getElementById("sum").innerHTML = sum.toFixed(2);
     }
-}
+})(A4 || (A4 = {}));
+//# sourceMappingURL=main.js.map
