cc3bed271a88762efc05610063e40eaf666722b4
LucasRohrberg <muffinamv@gmail.com>
2019-04-21 15:57:06 +0200

    tasks completed, sliders only work in 1 direction

--- task-4/js/main.js
+++ task-4/js/main.js
@@ -1,8 +1,18 @@
+/*
+Aufgabe: <4 - Eisdealer>
+Name: <Lucas Rohrberg>
+Matrikel: <260241>
+Datum: <21. April 2019, 15:56Uhr>
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
 var A4;
 (function (A4) {
     window.addEventListener("load", init);
     let sum = 0;
     let customerOrder = "";
+    let priceDelivery = 0;
+    let removed = 0;
     function init(_event) {
         console.log("Init");
         let fieldsets = document.getElementsByTagName("fieldset");
@@ -10,42 +20,81 @@ var A4;
             let fieldset = fieldsets[i];
             fieldset.addEventListener("change", handleChange);
         }
+        document.getElementById("checkOrder").addEventListener("click", checkOrderForMissingInformation);
     }
     function handleChange(_event) {
         let target = _event.target;
         let price;
+        let slider = document.getElementById(`${target.id}slider`);
         if (target.name == "ice" && target.checked == true) {
             price = Number(target.getAttribute("price"));
-            sum += price;
-            customerOrder = `<p id="${target.id}r">${target.value}, ${price}€</p>`;
+            sum += price * Number(slider.value);
+            updateSum(sum);
+            customerOrder = `<p id="${target.id}r">${target.value}: ${price * Number(slider.value)}€</p>`;
             updateCustomerOrder(customerOrder);
-            updateSum(sum);
-        }
-        if (target.name == "ice" && target.checked == false) {
-            price = Number(target.getAttribute("price"));
-            sum -= price;
-            document.getElementById(`${target.id}r`).outerHTML = "";
-            updateSum(sum);
         }
         if (target.name == "extras" && target.checked == true) {
             price = Number(target.getAttribute("price"));
             sum += price;
-            customerOrder = `<p id="${target.id}r">${target.value}, ${price}€</p>`;
-            updateCustomerOrder(customerOrder);
             updateSum(sum);
+            customerOrder = `<p id="${target.id}r">${target.value}: ${price}0€</p>`;
+            updateCustomerOrder(customerOrder);
+        }
+        if (target.name == "ice" && target.checked == false) {
+            price = Number(target.getAttribute("price"));
+            sum -= price * Number(slider.value);
+            updateSum(sum);
+            document.getElementById(`${target.id}r`).outerHTML = "";
         }
         if (target.name == "extras" && target.checked == false) {
             price = Number(target.getAttribute("price"));
             sum -= price;
-            document.getElementById(`${target.id}r`).outerHTML = "";
             updateSum(sum);
+            document.getElementById(`${target.id}r`).outerHTML = "";
+        }
+        if (target.name == "radiogroup1") {
+            document.getElementById("summaryContainer").innerHTML = target.value;
+        }
+        if (target.name == "radiogroup2") {
+            if (target.getAttribute("id") == "delivery" && priceDelivery == 0) {
+                priceDelivery = Number(target.getAttribute("price"));
+                sum += priceDelivery;
+                updateSum(sum);
+                customerOrder = `<p id="${target.id}r">${target.value}: ${priceDelivery}€</p>`;
+                updateCustomerOrder(customerOrder);
+                if (removed == 1)
+                    document.getElementById("pickupr").outerHTML = "";
+            }
+            else if (target.getAttribute("id") == "pickup" && priceDelivery > 0) {
+                sum -= priceDelivery;
+                updateSum(sum);
+                priceDelivery = 0;
+                customerOrder = `<p id="${target.id}r">${target.value}: ${priceDelivery}€</p>`;
+                updateCustomerOrder(customerOrder);
+                document.getElementById("deliveryr").outerHTML = "";
+                removed = 1;
+            }
+            else {
+                priceDelivery = 0;
+                customerOrder = `<p id="${target.id}r">${target.value}: ${priceDelivery}€</p>`;
+                updateCustomerOrder(customerOrder);
+                removed = 1;
+            }
         }
     }
     function updateCustomerOrder(_customerOrder) {
         document.getElementById("orderSummary").innerHTML += _customerOrder;
     }
     function updateSum(sum) {
-        document.getElementById("sum").innerHTML = sum.toFixed(2);
+        document.getElementById("sum").innerHTML = "Total: " + sum.toFixed(2) + "€";
+    }
+    function checkOrderForMissingInformation(_event) {
+        let requiredInputs = document.getElementsByTagName("input");
+        for (let i = 0; i < requiredInputs.length; i++) {
+            if (requiredInputs[i].hasAttribute("required") && requiredInputs[i].value == "") {
+                document.getElementById("missingInformation").innerHTML += `${requiredInputs[i].placeholder} is missing.<br>`;
+            }
+        }
     }
 })(A4 || (A4 = {}));
 //# sourceMappingURL=main.js.map
--- task-4/js/main.ts
+++ task-4/js/main.ts
@@ -1,8 +1,19 @@
+/*
+Aufgabe: <4 - Eisdealer>
+Name: <Lucas Rohrberg>
+Matrikel: <260241>
+Datum: <21. April 2019, 15:56Uhr>
+	
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
 namespace A4 {
     window.addEventListener("load", init);
 
     let sum: number = 0;
     let customerOrder: string = "";
+    let priceDelivery: number = 0;
+    let removed: number = 0;
 
     function init(_event: Event): void {
         console.log("Init");
@@ -12,37 +23,72 @@ namespace A4 {
             let fieldset: HTMLFieldSetElement = fieldsets[i];
             fieldset.addEventListener("change", handleChange);
         }
+
+        document.getElementById("checkOrder").addEventListener("click", checkOrderForMissingInformation)
     }
 
     function handleChange(_event: Event) {
         let target: HTMLInputElement = <HTMLInputElement>_event.target;
         let price: number;
+        let slider: HTMLProgressElement = <HTMLProgressElement>document.getElementById(`${target.id}slider`);
+
 
         if (target.name == "ice" && target.checked == true) {
             price = Number(target.getAttribute("price"));
-            sum += price;
-            customerOrder = `<p id="${target.id}r">${target.value}, ${price}€</p>`;
+            sum += price * Number(slider.value);
+            updateSum(sum);
+            customerOrder = `<p id="${target.id}r">${target.value}: ${price * Number(slider.value)}€</p>`;
             updateCustomerOrder(customerOrder);
-            updateSum(sum);
         }
-        if (target.name == "ice" && target.checked == false) {
-            price = Number(target.getAttribute("price"));
-            sum -= price;
-            document.getElementById(`${target.id}r`).outerHTML = "";
-            updateSum(sum);
-        }
+
         if (target.name == "extras" && target.checked == true) {
             price = Number(target.getAttribute("price"));
             sum += price;
-            customerOrder = `<p id="${target.id}r">${target.value}, ${price}€</p>`;
-            updateCustomerOrder(customerOrder);
             updateSum(sum);
+            customerOrder = `<p id="${target.id}r">${target.value}: ${price}0€</p>`;
+            updateCustomerOrder(customerOrder);
         }
+
+        if (target.name == "ice" && target.checked == false) {
+            price = Number(target.getAttribute("price"));
+            sum -= price * Number(slider.value);
+            updateSum(sum);
+            document.getElementById(`${target.id}r`).outerHTML = "";
+        }
+
         if (target.name == "extras" && target.checked == false) {
             price = Number(target.getAttribute("price"));
             sum -= price;
-            document.getElementById(`${target.id}r`).outerHTML = "";
             updateSum(sum);
+            document.getElementById(`${target.id}r`).outerHTML = "";
+        }
+        
+        if (target.name == "radiogroup1") {
+            document.getElementById("summaryContainer").innerHTML = target.value;
+        }
+
+        if (target.name == "radiogroup2") {
+            if (target.getAttribute("id") == "delivery" && priceDelivery == 0) {
+                priceDelivery = Number(target.getAttribute("price"));
+                sum += priceDelivery;
+                updateSum(sum);
+                customerOrder = `<p id="${target.id}r">${target.value}: ${priceDelivery}€</p>`;
+                updateCustomerOrder(customerOrder);
+                if (removed == 1) document.getElementById("pickupr").outerHTML = "";
+            } else if (target.getAttribute("id") == "pickup" && priceDelivery > 0) {
+                sum -= priceDelivery;
+                updateSum(sum);
+                priceDelivery = 0;
+                customerOrder = `<p id="${target.id}r">${target.value}: ${priceDelivery}€</p>`;
+                updateCustomerOrder(customerOrder);
+                document.getElementById("deliveryr").outerHTML = "";
+                removed = 1;
+            } else {
+                priceDelivery = 0;
+                customerOrder = `<p id="${target.id}r">${target.value}: ${priceDelivery}€</p>`;
+                updateCustomerOrder(customerOrder);
+                removed = 1;
+            }
         }
     }
 
@@ -51,6 +97,15 @@ namespace A4 {
     }
 
     function updateSum(sum: number) {
-        document.getElementById("sum").innerHTML = sum.toFixed(2);
+        document.getElementById("sum").innerHTML = "Total: " + sum.toFixed(2) + "€";
+    }
+
+    function checkOrderForMissingInformation(_event: Event) {
+        let requiredInputs = document.getElementsByTagName("input");
+        for (let i = 0; i < requiredInputs.length; i++) {
+            if (requiredInputs[i].hasAttribute("required") && requiredInputs[i].value == "") {
+                document.getElementById("missingInformation").innerHTML += `${requiredInputs[i].placeholder} is missing.<br>`;
+            }
+        }
     }
 }
