0f3ce697b9953216999cfd6e2ec6656432dacd60
LucasRohrberg <44026433+LucasRohrberg@users.noreply.github.com>
2019-05-05 10:43:17 +0200

    added server requirements

--- task-6/Server.js
+++ task-6/Server.js
@@ -1,1 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Http = require("http");
+var L06_Server;
+(function (L06_Server) {
+    console.log("Starting server");
+    let port = Number(process.env.PORT); // process.env gibt ein objekt zurück, das informationen zum environment des nutzers enthält; daraus wird der Port ausgelesen und in der Variable gespeichert
+    if (!port) // Wenn es keinen Port gibt, wird er auf 8100 gesetzt
+        port = 8100;
+    let server = Http.createServer(); // Macht aus dem PC einen Server, erstellt einen Server
+    server.addListener("request", handleRequest); // Wenn der Server eine Anfrage bemerkt, führt er handleRequest() aus
+    server.addListener("listening", handleListen); // Wenn der Server eine Veränderung auf dem zu beobachtenden Port feststellt, führt er handleListen() aus
+    server.listen(port); // Der Server beobachtet den Port 8100
+    function handleListen() {
+        console.log("Listening"); // Schreibt "Listening" in die Konsole (wenn Funktion ausgeführt wird)
+    }
+    function handleRequest(_request, _response) {
+        console.log("I hear voices!"); // Schreibt "I hear voices!" in die Konsole (wenn Funktion ausgeführt wird)
+        console.log(_request.url);
+        _response.setHeader("content-type", "text/html; charset=utf-8"); // Setzt name & value des Headers für _response
+        _response.setHeader("Access-Control-Allow-Origin", "*"); // Überschreibt vorher festgelegten Header? setHeader schreibt nur einen Header, writeHead kann mehrere haben
+        _response.write(_request.url); // Schreibt die URL der in _request gespeicherten Nachricht in den Output
+        _response.end(); // Beendet das Script und gibt das aktuelle Ergebnis zurück
+    }
+})(L06_Server || (L06_Server = {}));
 //# sourceMappingURL=Server.js.map
--- task-6/Server.ts
+++ task-6/Server.ts
@@ -1,28 +1,30 @@
 import * as Http from "http";
 
-namespace L05_Server {
-	console.log("Starting server");
-	let port: number = Number(process.env.PORT); // process.env gibt ein objekt zurück, das informationen zum environment des nutzers enthält; daraus wird der Port ausgelesen und in der Variable gespeichert
-	if (!port) // Wenn es keinen Port gibt, wird er auf 8100 gesetzt
-		port = 8100;
+namespace L06_Server {
 
-	let server: Http.Server = Http.createServer(); // Macht aus dem PC einen Server, erstellt einen Server
-	server.addListener("request", handleRequest); // Wenn der Server eine Anfrage bemerkt, führt er handleRequest() aus
-	server.addListener("listening", handleListen); // Wenn der Server eine Veränderung auf dem zu beobachtenden Port feststellt, führt er handleListen() aus
-	server.listen(port); // Der Server beobachtet den Port 8100
+  console.log("Starting server");
+  let port: number = Number(process.env.PORT); // process.env gibt ein objekt zurück, das informationen zum environment des nutzers enthält; daraus wird der Port ausgelesen und in der Variable gespeichert
+  if (!port) // Wenn es keinen Port gibt, wird er auf 8100 gesetzt
+    port = 8100;
 
-	function handleListen(): void { 
-		console.log("Listening"); // Schreibt "Listening" in die Konsole (wenn Funktion ausgeführt wird)
-	}
+  let server: Http.Server = Http.createServer(); // Macht aus dem PC einen Server, erstellt einen Server
+  server.addListener("request", handleRequest); // Wenn der Server eine Anfrage bemerkt, führt er handleRequest() aus
+  server.addListener("listening", handleListen); // Wenn der Server eine Veränderung auf dem zu beobachtenden Port feststellt, führt er handleListen() aus
+  server.listen(port); // Der Server beobachtet den Port 8100
 
-	function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { // Speichert eingehende Nachrichten & die dazugehörigen Server Antworten in _request bzw _response ab
-		console.log("I hear voices!"); // Schreibt "I hear voices!" in die Konsole (wenn Funktion ausgeführt wird)
+  function handleListen(): void {
+    console.log("Listening"); // Schreibt "Listening" in die Konsole (wenn Funktion ausgeführt wird)
+  }
 
-		_response.setHeader("content-type", "text/html; charset=utf-8"); // Setzt name & value des Headers für _response
-		_response.setHeader("Access-Control-Allow-Origin", "*"); // Überschreibt vorher festgelegten Header? setHeader schreibt nur einen Header, writeHead kann mehrere haben
+  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { // Speichert eingehende Nachrichten & die dazugehörigen Server Antworten in _request bzw _response ab
+    console.log("I hear voices!"); // Schreibt "I hear voices!" in die Konsole (wenn Funktion ausgeführt wird)
+    console.log(_request.url);
 
-		_response.write(_request.url); // Schreibt die URL der in _request gespeicherten Nachricht in den Output
+    _response.setHeader("content-type", "text/html; charset=utf-8"); // Setzt name & value des Headers für _response
+    _response.setHeader("Access-Control-Allow-Origin", "*"); // Überschreibt vorher festgelegten Header? setHeader schreibt nur einen Header, writeHead kann mehrere haben
 
-		_response.end(); // Beendet das Script und gibt das aktuelle Ergebnis zurück
-	}
+    _response.write(_request.url); // Schreibt die URL der in _request gespeicherten Nachricht in den Output
+
+    _response.end(); // Beendet das Script und gibt das aktuelle Ergebnis zurück
+  }
 }
